1e3491920836caec6901dcd999f167699d21ee5f
Liz Asraf <liz161@gmail.com>
2022-11-23 19:13:56 +0200

    web server works with readme

--- kube_re_server/app/routes.py
+++ kube_re_server/app/routes.py
@@ -1,9 +1,24 @@
 # request - asy way to convert Flask request form and args to route parameters
 from flask import request
-from app import app
-from app import metrics
-from kubernetes import client
+from prometheus_flask_exporter import PrometheusMetrics
+from kubernetes import client , config
 import logging
+# from app import app
+from flask import Flask
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+
+try:
+# if requested from outside the cluster
+    config.load_kube_config()
+except:
+    config.load_incluster_config()
+
+metrics = PrometheusMetrics(app)
+# static information as metric
+metrics.info('app_info', 'Application info', version='1.0.3')
+
 
 ############################################
 
@@ -21,7 +36,7 @@ logger.addHandler(console_handler)
 
 ############################################
 # setting of the cluster enviroment 
-v1 = client.CoreV1Api() # namespaces, services
+v1 = client.CoreV1Api() # namespaces, services, pods
 v2 = client.AppsV1Api() #deployments
 
 ############################################
@@ -29,62 +44,90 @@ v2 = client.AppsV1Api() #deployments
 def hello_world():
     try:
         logger.info('Empty request')
-        print('no requests') 
-        return ('no requests')   
+        toreturn = 'no requests \n\
+to get all the namespaces in the cluster please refer to /namespaces \n\
+to get all the services in some namespaces please refer to /services_<namespace> \n\
+to get all the deployments in some namespaces please refer to /deployments_<namespace> \n\
+to get all the pods in some deployment please refer to /pods_<deployment>\n\
+to get metrics of this server refer to /metrics'
+        print (toreturn)
+        toreturn = 'no requests <br>\
+to get all the namespaces in the cluster please refer to /namespaces <br>\
+to get all the services in some namespaces please refer to /services_namespace <br>\
+to get all the deployments in some namespaces please refer to /deployments_namespace <br>\
+to get all the pods in some deployment please refer to /pods_deployment' 
+        return (toreturn)   
     except Exception as error:
         logger.error("Oh no! an error has been occurred, the error is: " + str(error))
 
 ############################################
 # list of namespaces in the cluster
-@app.route('/namespaces', methods=['GET'])
+@app.route('/namespaces')
 def namespaces():
     try:
         logger.info('Get: list of namespaces')
         nameSpaceList = v1.list_namespace()
+        nameSpaceListt = ""
         for nameSpace in nameSpaceList.items:
             print(nameSpace.metadata.name)
+            nameSpaceListt += nameSpace.metadata.name + "<br>"
+        return ("list of namespaces:<br>" + nameSpaceListt)
     except Exception as error:
         logger.error("Oh no! an error has been occurred, the error is: " + str(error))
-
+        return ("error", 404 )
 ############################################
 # list of services in related namespace
-@app.route('/services-<namespace>', methods=['GET'])
+@app.route('/services_<namespace>', methods=['GET'])
 def services(namespace):
     try:
         logger.info('Get: list of services for namespace-%s',namespace)
         servicesList = v1.list_namespaced_service(namespace)
+        servicesListt = ""
         for service in servicesList.items:
-            print(service.metadata.name)    
+            print(service.metadata.name)
+            servicesListt += service.metadata.name + "<br>" 
+        return ("list of services for namespace-" + namespace + " are:<br>"+ servicesListt)   
     except Exception as error:
         logger.error("Oh no! an error has been occurred, the error is: " + str(error))
 
 ############################################
 # list of deployments in related namespace
-@app.route('/deployments-<namespace>', methods=['GET'])
+@app.route('/deployments_<namespace>', methods=['GET'])
 def deployments(namespace):
     try:
         logger.info('Get: deployments for namespace-%s',namespace )
         deploymentsList = v2.list_namespaced_deployment(namespace)
+        deploymentsListt = ""
         for deployment in deploymentsList.items:
             print(deployment.metadata.name)
-        return
+            deploymentsListt += deployment.metadata.name + "<br>" 
+        return ("list of deployments for namespace-" + namespace + " are:<br>"+ deploymentsListt)
     except Exception as error:
         logger.error("Oh no! an error has been occurred, the error is: " + str(error))
 
 ############################################
 
-@app.route('/pods-<deployment>', methods=['GET'])
+@app.route('/pods_<deployment>', methods=['GET'])
 def pods(deployment):
     try:
         logger.info('Get: pods for deployment-%s', deployment )        
         podsList = v1.list_pod_for_all_namespaces()
         deploymentsList = v2.list_deployment_for_all_namespaces()
+        for deployments in deploymentsList.items:
+            if deployments.metadata.name == deployment:
+                label = deployments.spec.selector._match_labels
+                deployments_match_labels = set(label.keys())
+        podsListt = ""
         for pod in podsList.items:
-            for deployment in deploymentsList.items:                
-                if pod.metadata.labels.app.kubernetes.io/instance == deployment.spec.selector.matchLabels.app.kubernetes.io/instance and pod.metadata.labels.app.kubernetes.io/name == deployment.spec.selector.matchLabels.app.kubernetes.io/name :
-                    print(pod.metadata.name)
+            pod_labels = set(pod.metadata.labels.keys())               
+            shared_keys = (deployments_match_labels).intersection(pod_labels)
+            same = set(o for o in shared_keys if label[o] == pod.metadata.labels[o])
+            if shared_keys == same and len(shared_keys)>0 :
+                print(pod.metadata.name)
+                podsListt += pod.metadata.name + "<br>" 
+        return ("list of pods for deployment-" + deployment + " are:<br>"+ podsListt)
     except Exception as error:
-        logger.error("Oh no! somthing went worng with the posts page, the error is: " + str(error))
+        logger.error("Oh no! an error has been occurred, the error is: " + str(error))
 
 ############################################
 
@@ -94,3 +137,7 @@ metrics.register_default(
         labels={'path': lambda: request.path}
     )
 )
+
+
+if __name__ == '__main__':
+   app.run()
--- kube_re_server/config.py
+++
@@ -1,5 +0,0 @@
-import os
-basedir = os.path.abspath(os.path.dirname(__file__))
-
-class Config(object):
-    config_files = os.getenv('KUBECONFIG', default='~/.kube/config')
